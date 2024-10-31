6fe8af867def486c482c04c2a2c7e0c18dc19afd
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-10-31 07:55:44 +0000

    Made the landing page

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
       <div className="xl:w-1/2 w-full flex flex-col gap-4 justify-center px-8 xl:items-start items-center xl:ps-0 pt-8 pb-4">
         <p className="xl:text-7xl md:text-6xl text-4xl text-white xl:text-start text-center">Enjoy the music, live life to the fullest.</p>
         <p className="xl:text-2xl md:text-xl text-md text-white px-1 xl:text-start text-center">Listen for your favourite music wherever you are.</p>
-        <button className="md:py-3 md:px-4 py-1 px-2 bg-gradient-to-r from-blue-400 font-semibold to-blue-900 rounded-3xl text-white xl:w-[250px] md:w-[200px] w-[150px] sm:text-[16px] text-[12px] flex justify-between items-center">Login with spotify <span>
+        <button className="md:py-3 md:px-4 py-1 px-2 bg-gradient-to-r from-blue-400 font-semibold to-blue-900 rounded-3xl text-white xl:w-[250px] md:w-[200px] w-[150px] md:text-[16px] text-[10px] flex justify-between items-center">Login with spotify <span>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevrons-right"><path d="m6 17 5-5-5-5" /><path d="m13 17 5-5-5-5" /></svg></span>
         </button>
       </div>
@@ -15,11 +15,11 @@ const Landing = () => {
         <div className="flex cursor-pointer">
           <picture>
             <source
-              srcSet={`${Headphones} 1200w, ${HeadphonesSmall} 600w`}
-              sizes="(max-width: 600px) 40vw, (max-width: 1200px) 15vw"
+              srcSet={`${Headphones} 1200w`}
+              sizes="(max-width: 600px) 40vw, (max-width: 1200px) 18vw"
               type="image/avif"
             />
-            <img src={HeadphonesSmall} width="500px" height="500px" alt="Description of the image" />
+            <img src={Headphones} width="500px" height="500px" alt="Description of the image" />
           </picture>
 
         </div>

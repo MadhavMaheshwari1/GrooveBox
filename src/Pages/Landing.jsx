c6a7cfdc9a649f145112eed4a6a895b125d302f7
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-11-05 12:56:19 +0000

    Made some changes

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -16,10 +16,10 @@ const Landing = () => {
             <Link to="/"><img src={RythmixLogo} alt="Logo" width="70px" height="70px" /></Link>
             <Link to="/" className="-mt-2">Rythmix</Link>
           </div>
-          <div className="flex flex-col gap-8 sm:items-start items-center sm:text-start text-center">
+          <div className="flex flex-col gap-8 md:items-start items-center md:text-start text-center w-full">
             <p className="xl:text-6xl sm:text-4xl text-2xl">Join the best music streaming service</p>
             <p className="xl:text-[8rem] sm:text-6xl text-4xl font-bold">Unleash your sonic story</p>
-            <Link to="/" className="flex gap-4 px-4 sm:py-4 py-2 xl:w-[240px] sm:w-[200px] w-[140px]  xl:text-3xl sm:text-xl text-[12px] items-center ml-2 border-2 border-white rounded-3xl hover:rounded-[5rem] hover:border-green-500 transition-all">Login with <img src={SpotifyLogo} alt="Spotify Logo" className="sm:w-[40px] w-[30px] sm:h-[40px] h-[30px]" /></Link>
+            <Link to="/" className="flex gap-4 px-4 md:py-3 py-2 xl:w-[230px] sm:w-[190px] w-[140px]  xl:text-3xl sm:text-xl text-[12px] items-center ml-2 border-2 border-white rounded-3xl hover:rounded-[5rem] hover:border-green-500 transition-all">Login with <img src={SpotifyLogo} alt="Spotify Logo" className="sm:w-[40px] w-[30px] sm:h-[40px] h-[30px]" /></Link>
           </div>
         </div>
 

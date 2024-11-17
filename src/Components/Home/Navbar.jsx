2241c1a4664e043f60789c62ea107c168adde56e
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-11-17 06:09:28 +0000

    Made some Changes

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -45,32 +45,39 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed z-40 w-[1880px] h-full" onClick={() => setShowMenu(false)}>
+      <div className="fixed z-40 max-w-[1880px] w-full h-full" onClick={() => setShowMenu(false)}>
+        <div className="absolute w-full h-full">
+          <div className="absolute h-[200px] w-[500px] rounded-xl bg-[#ff2c3a] z-[-1] right-1/2 filter blur-3xl mix-blend-multiply opacity-10 -top-[100px]"></div>
+          <div className="absolute h-[200px] w-[600px] rounded-xl bg-[#00fff7] z-[-1] right-1/3 filter blur-3xl mix-blend-multiply opacity-10 -top-[100px]"></div>
+          <div className="absolute h-[200px] w-[600px] rounded-xl bg-[#05d5ff] z-[-1] right-1/4 filter blur-3xl mix-blend-multiply opacity-10 -top-[100px]"></div>
+        </div>
         <div className={`flex py-8 w-full px-8 justify-between ${location.pathname === '/' ? 'hidden' : ''}`}>
-          <div className="flex w-full justify-between">
+          <div className="flex w-full justify-between items-center">
             <div className="flex gap-12">
-              <Link to="/home"><img src={RythmixLogo} alt="Logo" width="55px" height="55px" className="cursor-pointer" /></Link>
-              <div className="flex h-[60px] -mt-1 w-[620px] bg-transparent items-center rounded-xl border-[1px] border-gray-400 gap-4 relative">
+              <Link to="/home"><img src={RythmixLogo} alt="Logo" className="cursor-pointer md:h-[50px] md:w-[50px] h-[40px] w-[40px]" /></Link>
+              <div className="lg:flex h-[60px] -mt-1 lg:w-[620px] hidden bg-transparent items-center rounded-xl lg:border-[1px] border-gray-400 gap-4 relative">
                 <label htmlFor="search" className='cursor-pointer'><Search style={{ width: '2rem', height: '2rem', color: '#E5E7EB' }} className='ms-4' /></label>
-                <input type="text" id="search" onChange={e => inputHandler(e.target.value)} value={searchValue} placeholder='Search songs, albums, artists and podcasts' className=' text-gray-200 py-3 text-2xl w-[600px] bg-transparent placeholder:text-2xl placeholder:text-gray-200 placeholder:font-semibold focus:outline-none' />
-                <div className="absolute w-full h-full glass z-[-1]"></div>
+                <input type="text" id="search" onChange={e => inputHandler(e.target.value)} value={searchValue} placeholder='Search songs, albums, artists and podcasts' className='lg:block hidden text-gray-200 py-3 text-2xl w-[600px] bg-transparent placeholder:text-2xl placeholder:text-gray-200 placeholder:font-semibold focus:outline-none' />
+                <div className="absolute w-full h-full glass z-[-1] lg:block hidden"></div>
+
               </div>
             </div>
-            <div className="relative w-[80px] h-[80px] cursor-pointer flex justify-center items-center hover:scale-105 group transition-all duration-500">
-              {images && images.length > 0 && (
-                <button onClick={(e) => { setShowMenu(!showMenu); e.stopPropagation() }}><img src={images[1].url} alt="Profile Image" className='rounded-full w-[60px] h-[60px] backdrop-blur-lg' onContextMenu={(e) => e.preventDefault()} /></button>
-              )}
-              <div className="absolute w-full h-full glass top-0 z-[-1] rounded-full"></div>
-              {!showMenu && (<div className="opacity-0 group-hover:opacity-100 top-24 transition-all font-semibold duration-600 absolute text-white bg-[#2B2E35] rounded-md py-2 px-4 z-[1]">Madhav</div>)}
-              {showMenu && (
-                <div className="absolute w-[200px] h-[150px] glass flex items-center justify-center top-24 right-0 z-[1] rounded-xl py-4 text-white text-xl cursor-pointer">
-                  <div className="flex flex-col gap-4">
-                    <Link to={external_urls.spotify} className='flex justify-between w-[180px] mx-auto bg-[#2B2E35] py-2 px-3 rounded-xl'><UserRoundPen />Profile</Link>
-                    <button onClick={() => logoutHandler()} className='flex justify-between w-[180px] mx-auto bg-[#2B2E35] py-2 px-3 rounded-xl'><LogOut />Logout</button>
-                  </div>
-
-                </div>)
-              }
+            <div className="flex gap-4 items-center">
+              <label htmlFor="search" className='cursor-pointer'><Search style={{ width: '2rem', height: '2rem', color: '#E5E7EB' }} className='ms-4 lg:hidden block' /></label>
+              <div className="relative cursor-pointer flex justify-center items-center hover:scale-105 group transition-all duration-500">
+                {images && images.length > 0 && (
+                  <button onClick={(e) => { setShowMenu(!showMenu); e.stopPropagation() }}><img src={images[1].url} alt="Profile Image" className='rounded-full border-4 border-gray-400 md:w-[60px] md:h-[60px] h-[40px] w-[40px] backdrop-blur-lg' onContextMenu={(e) => e.preventDefault()} /></button>
+                )}
+                {!showMenu && (<div className="opacity-0 group-hover:opacity-100 top-20 transition-all font-semibold duration-600 absolute text-white bg-[#2B2E35] rounded-md py-2 px-4 z-[1]">Madhav</div>)}
+                {showMenu && (
+                  <div className="absolute lg:w-[200px] lg:h-[150px] lg:text-xl text-md w-[160px] h-[120px] glass flex items-center justify-center md:top-24 top-16 right-0 z-[1] rounded-xl py-4 text-white text-xl cursor-pointer">
+                    <div className="flex flex-col lg:gap-4 gap-2">
+                      <Link to={external_urls.spotify} className='flex justify-between lg:w-[180px] w-[130px] mx-auto bg-[#2B2E35] py-2 px-3 rounded-xl'><UserRoundPen />Profile</Link>
+                      <button onClick={() => logoutHandler()} className='flex justify-between lg:w-[180px] w-[130px] mx-auto bg-[#2B2E35] py-2 px-3 rounded-xl'><LogOut />Logout</button>
+                    </div>
+                  </div>)
+                }
+              </div>
             </div>
           </div>
         </div>
--- src/Components/Home/Sidebar.jsx
+++ src/Components/Home/Sidebar.jsx
@@ -26,24 +26,23 @@ const Navbar = () => {
   }
 
   return (
-    <div className={`fixed z-50 ${location.pathname === '/' ? 'hidden' : ''} cursor-pointer`}>
-      <div className="flex flex-col items-center">
-        <div className="mt-4 w-[120px] rounded-xl flex flex-col items-center py-[100px] text-white gap-6">
-          <Link to="/home" className={`flex flex-col items-center ${location.pathname.slice(1) === 'home' ? 'bg-[#2B2E35] font-semibold' : ''} py-3 px-5 rounded-xl`}><FaHouse fill={`${location.pathname.slice(1) === 'home' ? 'white' : '#2B2E35'}`} strokeWidth={40} style={{ width: '2rem', height: '2rem' }} />Home</Link>
-
-          <Link to="/library" className={`flex flex-col items-center ${location.pathname.slice(1) === 'library' ? 'bg-[#2B2E35] font-semibold' : ''} py-3 px-5 rounded-xl`}>
+    <div className={`fixed z-50 ${location.pathname === '/' ? 'hidden' : ''} cursor-pointer lg:top-0 lg:w-auto w-full bottom-0 `}>
+      <div className="flex w-full items-center">
+        <div className="mt-4 lg:text-lg text-[md] lg:w-[120px] w-full rounded-xl flex lg:flex-col flex-row items-center lg:justify-start justify-center lg:py-[100px] py-[20px] text-white gap-6">
+          <Link to="/home" className={`flex flex-col items-center gap-1 ${location.pathname.slice(1) === 'home' ? 'bg-[#2B2E35] font-semibold' : ''} md:py-3 md:px-5 px-3 py-2 rounded-xl`}><FaHouse fill={`${location.pathname.slice(1) === 'home' ? 'white' : '#2B2E35'}`} strokeWidth={40} className="w-5 h-5 md:w-7 md:h-7" />Home</Link>
+          <Link to="/library" className={`flex flex-col items-center ${location.pathname.slice(1) === 'library' ? 'bg-[#2B2E35] font-semibold' : ''}  md:py-3 md:px-5 px-3 py-2 rounded-xl `}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 -960 960 960"
-              fill={'white'}
+              fill="white"
               strokeWidth={40}
-              style={{ width: '2.5rem', height: '2.5rem' }}
+              className="w-6 h-6 md:w-8 md:h-8"
             >
-              <path
-                d="M500-360q42 0 71-29t29-71v-220h120v-80H560v220q-13-10-28-15t-32-5q-42 0-71 29t-29 71q0 42 29 71t71 29ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"
-              />
+              <path d="M500-360q42 0 71-29t29-71v-220h120v-80H560v220q-13-10-28-15t-32-5q-42 0-71 29t-29 71q0 42 29 71t71 29ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z" />
             </svg>
+
             Library</Link>
+
         </div>
       </div>
     </div>

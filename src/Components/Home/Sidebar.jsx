deedab95ff931df7b2eed55c322626fef97d3e51
MadhavMaheshwari1 <classwhitet@gmail.com>
2024-11-17 07:34:19 +0000

    Made some Changes

--- src/Components/Home/Sidebar.jsx
+++ src/Components/Home/Sidebar.jsx
@@ -26,17 +26,17 @@ const Navbar = () => {
   }
 
   return (
-    <div className={`fixed z-50 ${location.pathname === '/' ? 'hidden' : ''} cursor-pointer lg:top-0 lg:w-auto w-full bottom-0 `}>
+    <div className={`fixed z-50 ${location.pathname === '/' ? 'hidden' : ''} cursor-pointer lg:top-0 lg:w-auto w-full bottom-0`}>
       <div className="flex w-full items-center">
         <div className="mt-4 lg:text-lg text-[md] lg:w-[120px] w-full rounded-xl flex lg:flex-col flex-row items-center lg:justify-start justify-center lg:py-[100px] py-[20px] text-white gap-6">
-          <Link to="/home" className={`flex flex-col items-center gap-1 ${location.pathname.slice(1) === 'home' ? 'bg-[#2B2E35] font-semibold' : ''} md:py-3 md:px-5 px-3 py-2 rounded-xl`}><FaHouse fill={`${location.pathname.slice(1) === 'home' ? 'white' : '#2B2E35'}`} strokeWidth={40} className="w-5 h-5 md:w-7 md:h-7" />Home</Link>
-          <Link to="/library" className={`flex flex-col items-center ${location.pathname.slice(1) === 'library' ? 'bg-[#2B2E35] font-semibold' : ''}  md:py-3 md:px-5 px-3 py-2 rounded-xl `}>
+          <Link to="/home" className={`flex flex-col items-center gap-1 ${location.pathname.slice(1) === 'home' ? 'bg-[rgba(66,74,87,0.58)] font-semibold' : ''} md:py-3 md:px-5 px-3 py-2 rounded-xl`}><FaHouse fill={`${location.pathname.slice(1) === 'home' ? 'white' : '#2B2E35'}`} strokeWidth={40} className="w-7 h-7" />Home</Link>
+          <Link to="/library" className={`flex flex-col items-center ${location.pathname.slice(1) === 'library' ? 'bg-[rgba(66,74,87,0.58)] font-semibold' : ''}  md:py-3 md:px-5 px-3 py-2 rounded-xl `}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 -960 960 960"
               fill="white"
               strokeWidth={40}
-              className="w-6 h-6 md:w-8 md:h-8"
+              className="w-8 h-8"
             >
               <path d="M500-360q42 0 71-29t29-71v-220h120v-80H560v220q-13-10-28-15t-32-5q-42 0-71 29t-29 71q0 42 29 71t71 29ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z" />
             </svg>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,13 +6,30 @@ const Home = () => {
 
   return (
     <motion.div
-      className="max-w-[1880px] mx-auto"
+      className="max-w-[1880px] mx-auto lg:px-[130px] px-[30px] md:py-[140px] py-[120px]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: .5 }}
     >
       <div className="">
+        <div className="flex text-white gap-4 text-2xl overflow-x-hidden relative rounded-xl">
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Romance</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Chill</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Party</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Sad</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[180px] justify-center items-center whitespace-nowrap">Work Out</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Sleep</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Soul</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Study</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Pop</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Metalcore</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Kids</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Acoustic</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Club</button>
+          <button className="py-2 px-3 glass rounded-xl flex w-[130px] justify-center items-center whitespace-nowrap">Groove</button>
+          <div className="absolute w-[40px] h-[40px] right-0 z-50 rounded-full bg-cyan-500 filter blur-lg"></div>
+        </div>
       </div>
     </motion.div>
   );
